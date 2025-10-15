7c0f77a648df3494fe78e737003b95ebbfd57f61
srujith2006 <srujithraj06@gmail.com>
2025-10-15 23:20:52 +0530

    Update 4a.py

--- 4a.py
+++ 4a.py
@@ -6,3 +6,8 @@ series = pd.Series(data)
 
 print("Pandas Series:")
 print(series)
+
+# Convert Series to Python list 
+list_data = series.tolist() 
+print("Series as Python List:", list_data) 
+print("Type of list_data:", type(list_data)) 
